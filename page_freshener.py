883cfe3df666c90775483481324b138281da0549
MathCatsAnd <98661771+MathCatsAnd@users.noreply.github.com>
2023-02-28 15:35:27 -0800

    Fixed typo

--- page_freshener.py
+++ page_freshener.py
@@ -3,6 +3,6 @@ import streamlit as st
 def reset(page):
     if 'last_page' not in st.session_state:
         st.session_state.last_page = page
-    if st.last_page != page:
+    if st.session_state.last_page != page:
         st.session_state.clear()
         st.session_state.last_page = page
